fa22a4750299ee27f2ca0315d88101752e04b8ea
Daniel Van Den Heuvel <heuvel@student.chalmers.se>
2023-09-13 19:58:41 +0200

    Update Customer Module: route, model, controller
    * Create CRUD operations in controllers/customer.js \n* Link CRUD operations in routes/customer.js \n* Add customer attributes in models/customer.js

--- server/models/Customer.js
+++ server/models/Customer.js
@@ -6,9 +6,56 @@ const customerSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  email: String,
-  phone: String,
-  // Add other customer properties as needed
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Invalid email address format',
+    },
+    set: (email) => email.toLowerCase(),
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: (birthday) => {
+        const currentDate = new Date();
+        return birthday <= currentDate;
+      },
+      message: 'Invalid date of birth',
+    },
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  paymentMethods: [
+    {
+      type: String,
+    },
+  ],
+  favorites: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+    },
+  ],
+  loyaltyPoints: {
+    type: Number,
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model('Customer', customerSchema);
