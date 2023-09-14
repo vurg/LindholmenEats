2e0210446f978fd7a94611b6f361526ee81aba5e
Daniel Van Den Heuvel <heuvel@student.chalmers.se>
2023-09-14 18:57:13 +0200

    Modify Delivery Module
    Add deliveryStatus, transaction. Replace name with customer

--- server/models/Delivery.js
+++ server/models/Delivery.js
@@ -10,13 +10,24 @@ const deliverySchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    customerName: {
-        type: String,
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer',
         required: true,
     },
     timestamp: {
         type: Date,
         default: Date.now,
+    },
+    transaction: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Transaction',
+        required: true,
+    },
+    deliveryStatus: {
+        type: String,
+        enum: ['Pending', 'In Progress', 'Delivered', 'Cancelled'],
+        default: 'Pending',
     },
 });
 
