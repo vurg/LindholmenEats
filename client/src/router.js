986dc5e5a34be4c2d4b623c4c82f0c881d861897
Daniel Van Den Heuvel <heuvel@student.chalmers.se>
2023-10-03 11:03:23 +0200

    Create about page

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/HomePage.vue' // Make sure to import your Home Page component
 import MenuPage from '@/views/MenuPage.vue' // Import MenuPage.vue
+import AboutPage from '@/views/AboutPage.vue' // Import AboutPage.vue
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/menu',
       name: 'menu',
       component: MenuPage // Set MenuPage.vue as the component for the '/menu' route
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutPage
     },
     {
       path: '/:catchAll(.*)',
